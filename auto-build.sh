cff85cada18e290ff67db30b775a3d0359b92e13
CycleDM <cyclem0508@gmail.com>
2022-01-12 19:31:46 +0900

    add auto build script

--- auto-build.sh
+++ auto-build.sh
@@ -3,10 +3,11 @@ work_dir=$(dirname $(readlink -f "$0"))
 build_script=$work_dir/scripts/build-helper.sh
 push_script=$work_dir/scripts/push-helper.sh
 
-docker image prune -f
 while read line
 do
     echo $line
     sh $build_script $line
     sh $push_script ${line#*jre* }
 done < $work_dir/to-build-list
+
+docker image prune -f
--- scripts/push-helper.sh
+++ scripts/push-helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 server_type=$1
 game_version=$2
-latest_tag=$3
+add_tag=$3
 work_dir=$(dirname $(dirname $(readlink -f "$0")))
 
 # 检查参数
@@ -21,16 +21,16 @@ else
     echo "参数 $1, $2"
     exit 1
 fi
-if [ ! "$latest_tag" ]; then
+if [ ! "$add_tag" ]; then
     exit 0
 fi
 
-docker push cycledm/mcserver-$server_type:$latest_tag
+docker push cycledm/mcserver-$server_type:$add_tag
 result=$?
 if [ "$result" = "0" ]; then
-    echo "cycledm/mcserver-$server_type:$latest_tag 推送成功"
+    echo "cycledm/mcserver-$server_type:$add_tag 推送成功"
 else
-    echo "cycledm/mcserver-$server_type:$latest_tag 推送失败"
+    echo "cycledm/mcserver-$server_type:$add_tag 推送失败"
     echo "参数 $1, $2"
     exit 1
 fi

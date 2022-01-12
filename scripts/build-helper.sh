c0525b5eeb7285d958999f0ded807398b1a81d9c
CycleDM <cyclem0508@gmail.com>
2022-01-12 20:31:42 +0900

    update scripts

--- scripts/build-helper.sh
+++ scripts/build-helper.sh
@@ -4,6 +4,7 @@ server_type=$2
 game_version=$3
 add_tag=$4
 work_dir=$(dirname $(dirname $(readlink -f "$0")))
+image_name="cycledm/mcserver-$server_type"
 
 # 检查参数
 if [ ! "$jre_version" ] || [ ! "$server_type" ] || [ ! "$game_version" ]; then
@@ -17,17 +18,17 @@ cp $work_dir/dockerfile/Dockerfile $work_dir
 
 sed -i "s/cycledm\/dragonwell:jre17/cycledm\/dragonwell:$jre_version/g" $work_dir/Dockerfile
 if [ "$add_tag" ]; then
-    docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t cycledm/mcserver-$server_type:$game_version -t cycledm/mcserver-$server_type:$add_tag $work_dir
+    docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t $image_name$server_type:$game_version -t $image_name$server_type:$add_tag $work_dir
 else
-    docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t cycledm/mcserver-$server_type:$game_version $work_dir
+    docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t $image_name$server_type:$game_version $work_dir
 fi
-docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t cycledm/mcserver-$server_type:$game_version $work_dir
+docker build --build-arg SERVER_TYPE=$server_type --build-arg GAME_VERSION=$game_version -t $image_name$server_type:$game_version $work_dir
 if [ "$?" = "0" ]; then
-    echo "cycledm/mcserver-$server_type:$game_version 构建成功"
+    echo "$image_name$server_type:$game_version 构建成功"
     rm $work_dir/Dockerfile
     exit 0
 else
-    echo "cycledm/mcserver-$server_type:$game_version 构建失败"
+    echo "$image_name$server_type:$game_version 构建失败"
     echo "参数 $1, $2, $3"
     rm $work_dir/Dockerfile
     exit 1
--- scripts/push-helper.sh
+++ scripts/push-helper.sh
@@ -3,6 +3,7 @@ server_type=$1
 game_version=$2
 add_tag=$3
 work_dir=$(dirname $(dirname $(readlink -f "$0")))
+image_name="cycledm/mcserver-$server_type"
 
 # 检查参数
 if [ ! "$server_type" ] || [ ! "$game_version" ]; then
@@ -12,12 +13,12 @@ fi
 
 ########################################################################################
 unset result
-docker push cycledm/mcserver-$server_type:$game_version
+docker push $image_name$server_type:$game_version
 result=$?
 if [ "$result" = "0" ]; then
-    echo "cycledm/mcserver-$server_type:$game_version 推送成功"
+    echo "$image_name$server_type:$game_version 推送成功"
 else
-    echo "cycledm/mcserver-$server_type:$game_version 推送失败"
+    echo "$image_name$server_type:$game_version 推送失败"
     echo "参数 $1, $2"
     exit 1
 fi
@@ -25,12 +26,12 @@ if [ ! "$add_tag" ]; then
     exit 0
 fi
 
-docker push cycledm/mcserver-$server_type:$add_tag
+docker push $image_name$server_type:$add_tag
 result=$?
 if [ "$result" = "0" ]; then
-    echo "cycledm/mcserver-$server_type:$add_tag 推送成功"
+    echo "$image_name$server_type:$add_tag 推送成功"
 else
-    echo "cycledm/mcserver-$server_type:$add_tag 推送失败"
+    echo "$image_name$server_type:$add_tag 推送失败"
     echo "参数 $1, $2"
     exit 1
 fi
